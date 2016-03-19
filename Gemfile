b8da97272365e8c922417b5595e513fef0af5590
samarsaad950@yahoo.com <samarsaad950@yahoo.com>
2016-03-19 14:13:58 +0000

    create articles table, model and add validations

--- Gemfile
+++ Gemfile
@@ -48,4 +48,4 @@ end
 group :production do
   gem 'pg'
   gem 'rails_12factor'
-end  
+end
---
+++ db/migrate/20160316002938_create_articles.rb
@@ -0,0 +1,10 @@
+cd codeclass CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
